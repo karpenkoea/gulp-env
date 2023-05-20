702645a6fdd7f6d63cc95f8cd04ecaeeda92bc4e
karpenkoea <ps4karpenko@gmail.com>
2023-05-20 17:24:49 +0300

    fix svgSprite task

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ function watcher() {
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(fonts, svgSpriteTask, gulp.parallel(copy, html, scss, js, images));
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
